b58df4e3f65767c3cd9609d53dd932beae1fae09
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-04-18 10:26:45 -0300

    I added a coment in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <StrictMode> {/* usado somente em modo de desenvolvimento */}
     <App />
   </StrictMode>,
 )
