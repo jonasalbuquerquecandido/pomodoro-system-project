8b30dcece82656b635012445ecf5fc08e2cedfc7
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-04-21 14:46:55 -0300

    feat: i made some settings on the components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ export function App() {
 
   return (
     <>   {/* <> react fragment, abre uma div/elemento pai sem nomea-lo */}
-      <Heading />
+      <Heading attr={123} attr2='string'>Olá Mundo!</Heading>
+      
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate,
         fugit delectus. Quam facilis nobis, odit, nihil quod reprehenderit ipsam
