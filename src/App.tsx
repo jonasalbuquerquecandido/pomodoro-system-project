a180631cf92dbd79d1607c5a521b022517ae05eb
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-04-21 16:31:09 -0300

    feat: i added the variable colors  in the theme.css file

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import './styles/theme.css'
 import './styles/global.css' //importando o estilo
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>   {/* <> react fragment, abre uma div/elemento pai sem nomea-lo */}
       <Heading attr={123} attr2='string'>Olá Mundo!</Heading>
