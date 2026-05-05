795c02eb1c98f1e32a9e16d37561a4103875581c
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-05-05 20:17:00 -0300

    Icreated the standard structure of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import { Heading } from './components/Heading';
+import { Timer, Settings, History, House, Sun } from 'lucide-react';
 
-import './styles/theme.css' 
-import './styles/global.css' //importando o estilo
+import './styles/theme.css';
+import './styles/global.css'; //importando o estilo
 
 export function App() {
   return (
-    <>   {/* <> react fragment, abre uma div/elemento pai sem nomea-lo */}
-      <Heading>Olá Mundo!</Heading>
-      
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate,
-        fugit delectus. Quam facilis nobis, odit, nihil quod reprehenderit ipsam
-        beatae iusto qui error, illo maxime ipsa. Possimus nesciunt optio
-        corrupti.
-      </p>
+    <>
+      {' '}
+      {/* <> react fragment, abre uma div/elemento pai sem nomea-lo */}
+        <div className='container'>
+          <div className='content'>
+            <section>
+              LOGO
+            </section>
+          </div>
+        </div> 
+
+        <div className='container'>
+          <div className='content'>
+            <section>
+              MENU
+            </section>
+          </div>
+        </div> 
+
+        <div className='container'>
+          <div className='content'>
+            <section>
+              FORM
+            </section>
+          </div>
+        </div> 
+
+        <div className='container'>
+          <div className='content'>
+            <section>
+              FOOTER
+            </section>
+          </div>
+        </div> 
     </>
   );
 }
