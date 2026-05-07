c06294f91af535cf652b35a10ea3ec34952fa18b
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-05-07 19:42:14 -0300

    feat: I created a newContainer component and your css module

--- src/App.tsx
+++ src/App.tsx
@@ -2,59 +2,20 @@ import { Timer, Settings, History, House, Sun } from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css'; //importando o estilo
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      {' '}
-      {/* <> react fragment, abre uma div/elemento pai sem nomea-lo */}
-        <div className='container'>
-          <div className='content'>
-            <section>
-              LOGO
-            </section>
-          </div>
-        </div> 
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
 
-        <div className='container'>
-          <div className='content'>
-            <section>
-
-            <buttton>
-              <House />
-            </buttton>
-
-             <buttton>
-              <History />
-            </buttton>
-
-             <buttton>
-              <Settings />
-            </buttton>
-
-             <buttton>
-              <Sun />
-            </buttton>
-
-            </section>
-          </div>
-        </div> 
-
-        <div className='container'>
-          <div className='content'>
-            <section>
-              FORM
-            </section>
-          </div>
-        </div> 
-
-        <div className='container'>
-          <div className='content'>
-            <section>
-              FOOTER
-            </section>
-          </div>
-        </div> 
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+    
     </>
   );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,10 @@
 import styles from './Heading.module.css'
 
+import type { ReactNode} from "react"
+
 type HeadingProps = {
-  children: React.ReactNode;
-};
+    children: ReactNode;
+}
 
 export function Heading({ children }: HeadingProps) {
   return <h1 className={styles.heading}>{children}</h1>;
