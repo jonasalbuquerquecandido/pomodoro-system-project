9bfb88cc7b0ee64ac1d49dd4a1401604656bdd7f
jonasalbuquerquecandido <jonasalbuquerque.c@gmail.com>
2026-05-11 21:02:59 -0300

    feat: I created the html structure of main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Menu } from './components/Menu';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css'; //importando o estilo
@@ -15,7 +16,32 @@ export function App() {
       <Container>
         <Menu />
       </Container>
-    
+
+      <Container>
+        <CountDown />
+      </Container>    
+
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input type="text" id="input" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 </p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container> 
     </>
   );
 }
