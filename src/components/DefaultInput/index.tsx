d43b7fed35c4fa8f5edc214b0d6b4d954fd4898b
jonasalbuquerquecandido <jonasalbuquerque.c@gmail.com>
2026-05-24 15:52:42 -0300

    I added style to the main input

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -2,13 +2,14 @@ import styles from './styles.module.css';
 
 type DefaultInputProps = {
     id: string; 
-    labelText: string;
+    labelText?: string;
 } & React.ComponentProps<'input'>;
 
 export function DefaultInput({ id, type, labelText, ...rest }: DefaultInputProps) {
   return (      
   <>
-    {/*labelText? <label htmlFor={id}>{labelText}</label> : ''*/}
+  {/* condição ? verdadeiro : falso */}
+    {/*labelText  <label htmlFor={id}>{labelText}</label> : ''*/}
     <label htmlFor={id}>{labelText}</label>
     <input className={styles.input} type={type} id={id} {...rest}/>
   </>
