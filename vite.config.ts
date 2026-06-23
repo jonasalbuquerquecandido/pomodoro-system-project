e5b247db0daee308be9ea876bc643cf1fe57d558
jonasalbuquerquecandido <jonasalbuquerquecandido@gmail.com>
2026-06-22 21:46:43 -0300

    fix: github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/pomodoro-system-project/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
